35c9c74c95f0c8a1cc2f284683f706c08883f6d1
ConceptualHeap <rajatm1100@gmail.com>
2025-07-12 03:14:35 +0530

    second commit

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,5 @@ fn main() {
 
     let start = Instant::now();
 
-    println!("{}", Solution::int_to_roman(3479));
-    println!("{}", Solution::int_to_roman(58));
-    println!("{}", Solution::int_to_roman(1194));
-
     println!("Time elapsed : {}ms", start.elapsed().as_micros());
 }
